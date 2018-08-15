09a092b47ec277f275247a5ae62a33be880b81f9
johan <10015918@g.toiohomai.ac.nz>
2018-08-16 00:59:55 +1200

    q3 done

---
+++ COMP.5212-Assessment-Part-A/Question3/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3
+{
+    class Car
+    {
+        public String Colour { get; set; }
+        public String Make { get; set; }
+        public String Gearbox { get; set; }
+    }
+}
---
+++ COMP.5212-Assessment-Part-A/Question3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Car1 = new Car();
+            Console.WriteLine("Enter new car details");
+        
+            Console.WriteLine("Car colour: ");
+            Car1.Colour = Console.ReadLine();
+
+            Console.WriteLine("Car Make: ");
+            Car1.Make = Console.ReadLine();
+
+            Console.WriteLine("Gearbox type: ");
+            Car1.Gearbox = Console.ReadLine();
+
+            Console.WriteLine($"This car is a {Car1.Colour} {Car1.Make} with a {Car1.Gearbox} transmission");
+        }
+    }
+}
