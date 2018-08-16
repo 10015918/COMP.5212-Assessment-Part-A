0176052b21a11f1a24c04cd3f455061ea4f1b04b
johan <10015918@g.toiohomai.ac.nz>
2018-08-17 00:27:07 +1200

    stuck on q4

---
+++ COMP.5212-Assessment-Part-A/Question4/Person1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question4
+{
+    class Person1
+    {
+        String fullName;
+
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String YearOfBirth { get; set; }
+
+        public Person1 (String _FirstName, String _LastName, String _YearOfBirth)
+        {
+            FirstName = _FirstName;
+            LastName = _LastName;
+            YearOfBirth = _YearOfBirth;
+        }
+        public String GetFullName (String FirstName, String LastName, String fullName)
+        {
+            fullName = FirstName + LastName;
+            return fullName;
+        }
+
+    }
+}
---
+++ COMP.5212-Assessment-Part-A/Question4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person1 Person1 = new Person1("", "", "");
+            Console.WriteLine("Please enter the new person below:");
+
+            Console.WriteLine("First Name: ");
+            Person1.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Last Name");
+            Person1.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter year of birth");
+            Person1.YearOfBirth = Console.ReadLine();
+
+            GetFullName(FirstName, LastName);
+        }
+    }
+}
